55847bb06dcc15d0dad8e218164551dfa015291c
Kryrox <kryrox123@gmail.com>
2024-09-19 20:15:00 -0400

    presentacion

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Presentacion = () => {
+  return (
+    <div>
+      <h1>Tu Nombre</h1>
+      <img 
+        src="/holamundo/src/foto.png" 
+        alt="mi perfil" 
+      />
+      <p>
+        andres vela 
+      </p>
+    </div>
+  );
+};
+
+export default Presentacion;
