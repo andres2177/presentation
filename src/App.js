0a9724d5e754f9fb34e1ba7ea623b1503128e1fe
andres2177 <150204606+andres2177@users.noreply.github.com>
2024-09-25 02:54:06 +0000

    aumentar una titulo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Mi Presentación</h1>
+        <h1>mi presentacion</h1>
         <img src="src/foto.png" alt="perfil" />
         <p>
     Andres Vela Ilafaya
