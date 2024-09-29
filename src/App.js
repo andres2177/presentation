984b9689e00b1beb3ce95af397c5ebfa635341d7
andres2177 <150204606+andres2177@users.noreply.github.com>
2024-09-29 00:54:16 +0000

    cambiar imagen,modificar estilos y el app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import './App.css';
-import foto_personal from './images/foto.png';
+import foto_personal from './images/mi_foto.jpg';
+function Curriculum() {
+  return (
+    <div className="curriculum-container">
+    <div className="header">
+      <h1>Andrés Vela Ilafaya</h1>
+      <h2>Desarrollador de Software</h2>
+    </div>
+    
+    <div className="contact-info">
+      <p><strong>Email:</strong> ilafayaandres@gmail.com</p>
+      <p><strong>Teléfono:</strong> 74564025</p>
+      <p><strong>Nivel de Inglés:</strong> B2</p>
+    </div>
+    
+    <div className="hobbies">
+      <h3>Hobbies</h3>
+      <ul>
+        <li>Deportes</li>
+        <li>Videojuegos</li>
+        <li>Series</li>
+      </ul>
+    </div>
+  </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Mi Presentación</h1> 
-        <img src={foto_personal} alt="perfil" />
-        <p>
-    Andres Vela Ilafaya
-        </p>
-        <h2>Educacion</h2>
-        <ul>
-          <li>colegio U.E. Republica de Venezuela</li>
-          <li>tecnico superior en sistemas informaticos</li>
-          
-        </ul>
+      <img src={foto_personal} alt="Mi foto" className="profile-photo" />
+        <Curriculum />
       </header>
     </div>
   );
